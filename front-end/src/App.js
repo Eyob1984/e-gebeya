d793f5047c0627b3155014f344a04944f673f67b
Eyob1984 <tamireyob@gmail.com>
2021-03-29 21:37:57 -0700

    image rendered

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import HomeScreen from './Screens/HomeScreen'
+
+
+
 const App = () => {
   return (
     <>
     <Header />
     <main className="py-3">
       <Container>
-       <h1>Wel come Gebeya e-commerce</h1>
+      <HomeScreen />
       </Container>
     </main>
     <Footer />
